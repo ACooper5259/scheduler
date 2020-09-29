788b3cd4504cc01441f0e6d16718570215b700ef
ACooper5259 <audrey_cooper@me.com>
2020-09-29 14:17:01 -0700

    Interviewer list implemented, js, scss and stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+var classNames = require('classnames');
+
+export default function InterviewerList(props) {
+  const interviewersClass = classNames({
+    'interviewers__header': true,
+    'interviewers__list': true
+  })
+  
+  const interviewerList = props.interviewers.map (interviewer => {
+    return(<InterviewerListItem
+      key= {interviewer.id}
+      name = {interviewer.name}
+      avatar = {interviewer.avatar}
+      selected = {interviewer.id === props.interviewer}
+      setInterviewer = {props.setInterviewer}
+      />
+    )
+    })
+  
+  return (
+      <section className="interviewers">
+        <h4 className="interviewers__header text--light">Interviewer</h4>
+        <ul className="interviewers__list">{interviewerList}</ul>
+      </section>
+  )
+}
