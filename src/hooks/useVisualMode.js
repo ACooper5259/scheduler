94e0484e2ef39f7ce1c221a3c2671da7573330af
ACooper5259 <audrey_cooper@me.com>
2020-10-04 17:33:50 -0700

    Error scenario implemented: changes to useVisualModes hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,22 +3,20 @@ import React, {useState} from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+  
   function transition (newMode, replace = false) {
     if (replace) {
-      return (mode)
-    }
+      setMode(newMode)
+    } else {
     setHistory(current => [...current, newMode]);
-    return setMode(newMode);
-  }
+    setMode(newMode);
+   }
+  };
   function back(){
-    if (history.length > 1) {
-      history.pop();
+    history.pop();
+    if (history.length >= 1) {
+      setMode(history[history.length-1]);
     }
-    return setMode(history[history.length-1]);
-  }
+  };
   return { mode, transition, back };
-}
-
-
-
-
+};
